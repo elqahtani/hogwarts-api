546751638867ba87a1ca813a5d62143c3fa4a57a
Huda Ridwan <huda.ridwan@mamiteam.com>
2020-02-29 08:28:21 +0700

    Bump Version Update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/api/', (req, res) => {
 });
 
 app.get('/api/version', (req, res) => {
-   res.json({ message: 'Version : 0.0.4' });
+   res.json({ message: 'Version : 0.0.5' });
 });
 
 app.get("/api/houses", (req, res) => {
